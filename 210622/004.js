343578fcb4b2e6f8cdaea1b468cf3a62e6729399
blueskyEunhan <76895039+blueskyEunhan@users.noreply.github.com>
2021-06-22 23:53:20 +0900

    210622

---
+++ 210622/004.js
@@ -0,0 +1,38 @@
+/*console.log("Visual Studio Code로 코드 실행해보기");
+var a = 5;
+var b =2;
+console.log(a+b);*/
+
+var family={
+    'address':'Seoul',
+    members:{},
+    addFamily: function(age,name,role){
+        this.members[role]={
+            age:age,
+            name:name
+        };
+    },
+    getHeadCount: function(){
+        return Object.keys(this.members).length;
+    }
+};
+family.addFamily(30,'chloe','aunt');
+family.addFamily(3,'lyn','niece');
+family.addFamily(10,'dangdangi','dog');
+
+var printMembers=function(){
+    var members=family.members;
+    for(role in members){
+        console.log('role=>'+role+' , name=>'+members[role].name+' , age=>'+members[role].age);
+    }
+};
+printMembers();
+
+var members=family.members;
+members['nephew']={age:3,name:'hyun'};
+members.niece={age:5,name:'lyn'};
+delete members.aunt;
+delete members['dog'];
+printMembers();
+
+// console.log(family.getHeadCount());
